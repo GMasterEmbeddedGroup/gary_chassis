633171505ff2526598d8b85692846a64d5f32068
myaki-moe <i@myaki.moe>
2023-02-13 11:48:35 +0800

    fix: tweak max speed settings, remove unused code

--- src/mode_changer.cpp
+++ src/mode_changer.cpp
@@ -13,8 +13,7 @@ public:
     ModeChanger() : Node("chassis_task"){
          rc_sub = this->create_subscription<gary_msgs::msg::DR16Receiver>("/remote_control",rclcpp::SystemDefaultsQoS(),
                                                           std::bind(&ModeChanger::rc_callback,this,std::placeholders::_1));
-         twist_pub = this->create_publisher<geometry_msgs::msg::Twist>("/topic_cmd",rclcpp::SystemDefaultsQoS());
-         //timer = this->create_wall_timer(100ms,std::bind(&ModeChanger::time_callback,this));
+         twist_pub = this->create_publisher<geometry_msgs::msg::Twist>("/chassis_solver/cmd_vel",rclcpp::SystemDefaultsQoS());
     }
 private:
     void rc_callback(gary_msgs::msg::DR16Receiver::SharedPtr msg)
@@ -27,9 +26,9 @@ private:
         }
         else if(RC_control.sw_right == RC_control.SW_MID)
         {
-        twist.linear.x = RC_control.ch_left_x/660 * 50;
-        twist.linear.y = RC_control.ch_left_y/660 * 50;
-        twist.angular.z = RC_control.ch_right_x/660 * 50;
+        twist.linear.x = RC_control.ch_left_x * 2;
+        twist.linear.y = RC_control.ch_left_y * 2;
+        twist.angular.z = RC_control.ch_right_x * 2;
         }
         std::map<std::string,double> chassis_speed;
         chassis_speed.insert(std::make_pair("vx",twist.linear.x));
@@ -39,13 +38,10 @@ private:
         twist_pub -> publish(twist);
         
     }
-    /*void time_callback()
-    {
 
-    }*/
     rclcpp::Subscription<gary_msgs::msg::DR16Receiver>::SharedPtr rc_sub;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub;
-    //rclcpp::TimerBase::SharedPtr timer;
+
 };
 
 int main(int argc, char * argv[]){
